fe4186df39b91c00d1277986911626305ff68c82
Alessandra-rib <83190264+Alessandra-rib@users.noreply.github.com>
2021-04-25 20:14:42 -0300

    Create SistemaOperacional.java

---
+++ SistemaOperacional.java
@@ -0,0 +1,16 @@
+package ListaAvaliativa;
+
+public class SistemaOperacional {
+
+	private String nome;
+
+	public SistemaOperacional(String nome) {
+		this.nome = nome;
+	}
+	
+	//Getter and Setters
+	public String getNome() {
+		return nome;
+	}
+	
+}
